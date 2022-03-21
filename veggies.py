ada112d71f47ee8402df4670d17d95ba835e9107
htt1103 <18027532169@163.com>
2022-03-21 08:40:11 +0800

    veggies order system

---
+++ veggies.py
@@ -0,0 +1,10 @@
+from sympy import ordered
+
+
+veg_price={'cabbage':2.87,'carrot':1.59,'spinach':3.33,'asparagus':2.54,'artichoke':3.00,'lettuce':2.43}
+order={'cabbage':2,'artichoke':5}
+
+total=0
+for veggie,auantity in order.items():
+    total+=veg_price[veggie]*quantity
+print(total)
